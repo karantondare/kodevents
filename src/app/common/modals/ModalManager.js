5bb9dd5661db2643acedbeaf1c1dc30885176faa
karantondare <karantondare.study@gmail.com>
2021-03-21 14:32:12 +0530

    changes to ModalManager

--- src/app/common/modals/ModalManager.js
+++ src/app/common/modals/ModalManager.js
@@ -1,11 +1,9 @@
 import React from "react";
 import { useSelector } from "react-redux";
-import TestModal from "../../../features/sandbox/TestModal";
 import LoginForm from "../../../features/auth/LoginForm";
 
 export default function ModalManager() {
   const modalLookup = {
-    TestModal,
     LoginForm,
   };
 
